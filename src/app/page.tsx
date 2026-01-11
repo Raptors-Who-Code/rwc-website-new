f3097c15ba2801a9fa0c0387a35d9414bed6c9f4
EnderSP <endonathansp@gmail.com>
2026-01-11 01:51:49 +0000

    organized code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,6 @@ function Hero() {
                         </Button>
                         
                          <Button asChild size="lg" variant="outline" className="gap-2"> 
-
                             <a href="/events">
                                 <CalendarDays className="w-5 h-5" />
                                 Explore Events
